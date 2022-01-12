0ef8dfd9636dfefd88328a3ba28f4fcd9d7c2df4
JCMP92 <arq_juanmp@outlook.com>
2022-01-12 09:31:40 -0600

    8/8

--- JS/code.js
+++ JS/code.js
@@ -76,4 +76,12 @@
       console.table(fullName);
       // 8. Reduce Exercise
       // Sum up the instances of each of these
-      const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+      const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'truck', 'truck', 'van', 'bike', 'bike', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van' ];
+
+      const vehicle = data.reduce(function (obj, item) {
+        if(!obj[item]){
+          obj[item]=0;
+        }
+        obj[item]++;
+        return obj;
+      }, {});
